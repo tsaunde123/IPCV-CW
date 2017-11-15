1a5956ed4a685976230e2f1c7a1d5671d5cc66e4
TristanSaunders <tristansaundersfr@gmail.com>
2017-11-15 16:16:49 +0000

    changed some circle detection parameters

--- face.cpp
+++ face.cpp
@@ -20,7 +20,8 @@ using namespace std;
 using namespace cv;
 
 /** Function Headers */
-void detectAndDisplay( Mat frame );
+void detectAndDisplay( Mat &frame, Mat &output );
+void detectCircles(Mat &frame, Mat &output);
 
 /** Global variables */
 String cascade_name = "dartcascade/cascade.xml";
@@ -32,21 +33,23 @@ int main( int argc, const char** argv )
 {
        // 1. Read Input Image
 	Mat frame = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+	Mat frame_output = frame;
 
 	// 2. Load the Strong Classifier in a structure called `Cascade'
 	if( !cascade.load( cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
 
 	// 3. Detect Faces and Display Result
-	detectAndDisplay( frame );
+	detectAndDisplay( frame, frame_output );
+	detectCircles(frame, frame_output);
 
 	// 4. Save Result Image
-	imwrite( "detected.jpg", frame );
+	imwrite( "detected.jpg", frame_output );
 
 	return 0;
 }
 
 /** @function detectAndDisplay */
-void detectAndDisplay( Mat frame )
+void detectAndDisplay( Mat &frame, Mat &output )
 {
 	std::vector<Rect> faces;
 	Mat frame_gray;
@@ -64,7 +67,44 @@ void detectAndDisplay( Mat frame )
        // 4. Draw box around faces found
 	for( int i = 0; i < faces.size(); i++ )
 	{
-		rectangle(frame, Point(faces[i].x, faces[i].y), Point(faces[i].x + faces[i].width, faces[i].y + faces[i].height), Scalar( 0, 255, 0 ), 2);
+		rectangle(output, Point(faces[i].x, faces[i].y), Point(faces[i].x + faces[i].width, faces[i].y + faces[i].height), Scalar( 0, 255, 0 ), 2);
 	}
 
+}
+
+void detectCircles(Mat &frame, Mat &output )
+{
+	Mat gray_image;
+	int rows;
+	int cols;
+	cv::Size size = gray_image.size();
+	rows = size.height;
+	cols = size.width;
+
+	/// Convert it to gray
+  cvtColor( frame, gray_image, CV_BGR2GRAY );
+
+  /// Reduce the noise so we avoid false circle detection
+  //GaussianBlur( gray_image, gray_image, Size(5, 5), 2, 2 );
+	GaussianBlur( gray_image, gray_image, Size(9, 9), 2, 2 );
+
+  vector<Vec3f> circles;
+
+  /// Apply the Hough Transform to find the circles
+  //HoughCircles( gray_image, circles, CV_HOUGH_GRADIENT, 1.2, gray_image.rows/12, 170, 100, 0, 0 );
+	//HoughCircles( gray_image, circles, CV_HOUGH_GRADIENT, dp, minDist, param1, param2, minrad, maxrad );
+	HoughCircles( gray_image, circles, CV_HOUGH_GRADIENT, 1.1, 30, 200, 70, 0, 0 );
+
+	std::cout << circles.size() << std::endl;
+
+  /// Draw the circles detected
+  for( size_t i = 0; i < circles.size(); i++ )
+  {
+      Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+      int radius = cvRound(circles[i][2]);
+      // circle center
+      circle( output, center, 3, Scalar(0,255,0), -1, 8, 0 );
+      // circle outline
+      circle( output, center, radius, Scalar(0,25*i,255), 3, 8, 0 );
+   }
 }
